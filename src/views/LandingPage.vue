36f0d9f54ccf27817f39a9c23ddd773cc12a44e0
saviorbp <72775597+saviorbp@users.noreply.github.com>
2023-05-19 22:52:50 -0300

    melhorias

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,36 +1,63 @@
 <template>
   <div id="conteudo" class="d-flex">
-    <v-navigation-drawer
-        permanent
-        expand-on-hover
-        dark
-      >
-        <v-list>
-          <v-list-item class="px-2">
-            <v-list-item-avatar>
-              <v-icon x-large>mdi-format-list-bulleted-square</v-icon>
-            </v-list-item-avatar>
-          </v-list-item>
-        </v-list>
+    <v-navigation-drawer permanent expand-on-hover dark>
+      <v-list>
+        <v-list-item class="px-2">
+          <v-list-item-avatar>
+            <v-icon x-large>mdi-format-list-bulleted-square</v-icon>
+          </v-list-item-avatar>
+        </v-list-item>
+      </v-list>
 
-        <v-divider></v-divider>
+      <v-divider></v-divider>
 
-        <v-list
-          nav
-          dense
-        >
-          <v-list-item link>
-            <v-list-item-icon>
-              <v-icon>mdi-code-braces</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Configuration Options in JavaScript</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
+      <v-list nav dense>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-code-braces</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title
+            >Configuration Options in JavaScript</v-list-item-title
+          >
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <v-text-field
+        v-model="trace.x"
+        label="X"
+        outlined
+        number
+        dense
+        class="mx-2"
+      ></v-text-field>
+      <v-text-field
+        v-model="trace.y"
+        label="Y"
+        outlined
+        dense
+        class="mx-2"
+      ></v-text-field>
+      <v-text-field
+        v-model="trace.type"
+        label="Type"
+        outlined
+        dense
+        class="mx-2"
+      ></v-text-field>
+      <v-text-field
+        v-model="trace.name"
+        label="Name"
+        outlined
+        dense
+        class="mx-2"
+      ></v-text-field>
 
-    <div ref="myDiv" class="plot">
-      <!-- Plotly chart will be drawn inside this DIV -->
-    </div>
+      <v-btn @click="update()" class="mx-2">Update</v-btn>
+      <div ref="myDiv" class="plot">
+        <!-- Plotly chart will be drawn inside this DIV -->
+      </div>
+    </v-main>
   </div>
 </template>
 
@@ -41,35 +68,61 @@ import Plotly from "plotly.js-dist/plotly";
 export default {
   data() {
     return {
-      data: [
-        {
-          x: ["2020-10-04", "2021-11-04", "2023-12-04"],
-          y: [90, 40, 60],
-          type: "scatter",
-        },
-      ],
+      trace: {
+        x: [],
+        y: [],
+        type: "",
+        name: "",
+      },
+      data: [],
       layout: {
         title: "Scroll and Zoom",
         showlegend: false,
       },
     };
   },
-  mounted() {
-    Plotly.newPlot(this.$refs.myDiv, this.data, this.layout, {
-      scrollZoom: true,
-    });
+  methods: {
+    // Updates the chart with the current user input
+    update() {
+      // Convert comma-separated string of x and y values to arrays
+      this.trace.x = this.trace.x.split(",").map(parseFloat);
+      this.trace.y = this.trace.y.split(",").map(parseFloat);
+
+      // Build trace data array
+      this.buildData();
+
+      // Update the chart
+      this.updateChart();
+    },
+
+    // Validates user input
+    buildData() {
+      this.data = [
+        {
+          x: this.trace.x,
+          y: this.trace.y,
+          type: this.trace.type,
+          name: this.trace.name,
+        },
+      ];
+    },
+    updateChart() {
+      Plotly.react(this.$refs.myDiv, this.data, this.layout, {
+        scrollZoom: true,
+      });
+    },
   },
 };
 </script>
 
-<style>
 
-.plot{
-height: 500px;
-width: 1000px;
+<style>
+.plot {
+  height: 500px;
+  width: 1000px;
 }
 
-.v-list-item{
-    overflow-wrap: break-word !important;
+.v-list-item {
+  overflow-wrap: break-word !important;
 }
 </style>
